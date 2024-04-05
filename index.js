38d2f21fad172af07868cab33f26beccb2d6bcf4
nithyarg <nithyaasbsc@gmail.com>
2024-04-05 20:42:18 +0530

    8 complted

---
+++ index.js
@@ -0,0 +1,14 @@
+
+        // Echoin g JavaScript
+        // line 2
+        // line 3
+    alert("Hello Nithya");
+
+    alert(4+5);
+
+    /*
+    console logging
+    */
+
+    console.log("Hello i am building JS!"); 
+    
