69cbac266d83946d10e50f1d0d1d6e77f7accc66
nithyarg <nithyaasbsc@gmail.com>
2024-04-06 22:14:23 +0530

    9 complted

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+// let name = "Nithya"
 
-        // Echoin g JavaScript
-        // line 2
-        // line 3
-    alert("Hello Nithya");
+// // var msg = "Hello my name is Name, I'm learning Javascript";
+// let msg = "Hello my name is " + name + " I'm learning Javascript";
 
-    alert(4+5);
+// // Alerting 
+// alert(msg);
 
-    /*
-    console logging
-    */
+// // logging
+// console.log(msg);
 
-    console.log("Hello i am building JS!"); 
-    
+// let name = "nithya";
+// console.log(name);
+
+let number1 = 5;
+let number2 = 45;
+
+let result = number1+ number2;
+console.log(result);
+
+/* 
+   rules:
+    1.no javascript keywords name;(let if=5; error varum if -the keyword)
+    2.should not start with number;(let 5number=5)
+    3.no space & no -
+    4.its case-Sensitive (let=fullName=5; let=FullName=5;)
+    5. use meaningful names
+*/
