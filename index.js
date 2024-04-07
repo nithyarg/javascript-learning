e3081802fcd4347753d76e59fbb2bc199889a537
nithyarg <nithyaasbsc@gmail.com>
2024-04-07 21:18:34 +0530

    28 complted

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
- // conditional statements
+// switch case 
+ let marks = 90;
 
-//  let isRaining = Ture;
-//  let iscloudy = false;
-
-//  if(isRaining || iscloudy){
-//     console.log("don't forget to take umbrella!");
-//  }
-//  else {
-//     console.log("sky is normal! enjoy the weather!");
-//  }
-
-
-
-
-// if hour is between 12Am to 1Pm -> Good Morning
-// Else if hour is between 1Pm to 5PM -> Good Afternoon
-//Else hour is between 5PM to 12AM -> good evening
-
-let hour = 14;
- 
-
-if(hour >= o && hour <= 13){
-    console.log("Good Morning");
-}
-else if (hour >= 13 && hour <= 17){
-    console.log("Good Afternoon");
-} 
-else
-    console.log("good evening");
+ switch(true){
+    case marks > 50:
+        console.log("pass");
+        break;
+    case marks >90:
+        console.log("super grade");
+        break;
+    case marks < 50:
+        console.log("failed!");
+        break; 
+    default:
+        console.log("unknown grade");           
+ }
