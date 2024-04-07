d3c235e9152a992d405132baac4d3aa97a503ece
nithyarg <nithyaasbsc@gmail.com>
2024-04-07 20:49:32 +0530

    27 complted

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-// ternary operators
+ // conditional statements
 
-// If person's age is more than 18,
-// they are 'Adult' category, otherwise,
-// They are a 'child' category
+//  let isRaining = Ture;
+//  let iscloudy = false;
 
-let age =24;
+//  if(isRaining || iscloudy){
+//     console.log("don't forget to take umbrella!");
+//  }
+//  else {
+//     console.log("sky is normal! enjoy the weather!");
+//  }
 
-let type = age > 18 ? "Adult Ticket" : "child Ticket"
 
- console.log(type);
 
- // condition ? 'value1' : 'value2' 
 
+// if hour is between 12Am to 1Pm -> Good Morning
+// Else if hour is between 1Pm to 5PM -> Good Afternoon
+//Else hour is between 5PM to 12AM -> good evening
+
+let hour = 14;
+ 
+
+if(hour >= o && hour <= 13){
+    console.log("Good Morning");
+}
+else if (hour >= 13 && hour <= 17){
+    console.log("Good Afternoon");
+} 
+else
+    console.log("good evening");
