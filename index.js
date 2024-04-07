e721d14fd835cfbf55a23ad8c93f3e46b42374dd
nithyarg <nithyaasbsc@gmail.com>
2024-04-07 08:22:20 +0530

    11 complted

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-// let name = "Nithya"
+const name = "nithya";
+// name ="nithi";
 
-// // var msg = "Hello my name is Name, I'm learning Javascript";
-// let msg = "Hello my name is " + name + " I'm learning Javascript";
+let msg ="Hello my name is "+ name + "I'm learning javascript";
 
-// // Alerting 
-// alert(msg);
-
-// // logging
-// console.log(msg);
-
-// let name = "nithya";
-// console.log(name);
-
-let number1 = 5;
-let number2 = 45;
-
-let result = number1+ number2;
-console.log(result);
-
-/* 
-   rules:
-    1.no javascript keywords name;(let if=5; error varum if -the keyword)
-    2.should not start with number;(let 5number=5)
-    3.no space & no -
-    4.its case-Sensitive (let=fullName=5; let=FullName=5;)
-    5. use meaningful names
-*/
+//logging 
+console.log(msg);
+ 
