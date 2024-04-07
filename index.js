34c0f2f005f70deab69c23be9d8d8a3b952f2fb5
nithyarg <nithyaasbsc@gmail.com>
2024-04-07 14:18:52 +0530

    16 complted

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
  
-//object
+// //object
  
- let person = {
-    name: "value",
-    age : 22,
-    gender :"Female",
-    ddress : "Chennai, Tamil Nadu, India"
- } 
- //object
+//  let person = {
+//     name: "value",
+//     age : 22,
+//     gender :"Female",
+//     ddress : "Chennai, Tamil Nadu, India"
+//  } 
+//  //object
 
- // Dot Notation
- console.log(person.age)
+//  // Dot Notation
+//  console.log(person.age)
 
- //bracket notation
- console.log(person['gender'])
+//  //bracket notation
+//  console.log(person['gender'])
 
+// let favcolors =["red","white","black"]
 
+// console.log(favcolors)
  
+function greetUser(firstName, lastName){
+    //set of statements
+    let name = "nithi";
+    let msg = "hello"  +  firstName + "" + lastName  +  "I'm learning javascript";
+    console.log(msg)
+}
+
+function add(number1,number2){
+    console.log(number1 + number2)
+
+    // -
+    // *
+    // /
+}
+add(45,62)
+
+// greetUser()
