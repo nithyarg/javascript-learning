ad3f02f146d2ae13e8a4fcf0e9b3f679bf0a0b67
nithyarg <nithyaasbsc@gmail.com>
2024-04-12 20:20:35 +0530

    52 complted

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-// Adding element to Javascript Array
-// const number =[4,5,6]
+//Finding element in javascript array
 
-// console.log(number)
+const items =["Apple","Lemon","Grapes","Apple"]
 
+console.log(items.indexOf("Apple"));
 
-const number =["Apple","Lemon","Grapes"]
+console.log(items.lastIndexOf("Apple"));
 
-// number[3]= "Orange";
-
-
-
-// push 
-number.push("Orange","Strawberries")
-
-//unshift
-number.unshift("Gauva")
-
-//splice
-number.splice(3,0,"Watermelons","Bananas")
-
-  
+console.log(items.indexOf("Grapes") !== -1) //or
 
 
+console.log (items.includes("Banana"));
 
 
+// finding element js Reference array
+
+ const orders = [
+    {id:1,item:"Smartphones", quantity:1},
+    {id:2,item:"Maagi", quantity:3},
+ ];
+
+  let result = orders.find(function(order){
+   return order.item === "Maagi"
+  })
 
 
-console.log(number)
+  console.log(result);
