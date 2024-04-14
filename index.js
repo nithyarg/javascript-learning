02c29761361bcaaf4266ef2ce154fdd47e21fd44
nithyarg <nithyaasbsc@gmail.com>
2024-04-14 20:32:09 +0530

    68 completed

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
- // javascript arrays - reduce method 
- const donations = [45,78,95,65,4851];
-
- donations.reduce(function (previousVal, currentVal){
-    // logic
-    console.log("previousVal", previousVal);
-    console.log("currentVal", currentVal);
-
-    return previousVal + currentVal;
- });
-
- let totalDonations= donations.reduce(function(previousval,currentVal){
-    return previousval+currentVal;
- });
-
- console.log("totalDonations",totalDonations);
+// javascript:
+// sort(),find() vs filter vs map() vs reduce
+  
+const numbers =[34,4,65,12,78];
 
 
- const shoppingCart = [
-   {id: 1, item:"organic milk",cost: 45},
-   {id: 2, item:"bread",cost: 20},
-   {id: 3, item:"maagi",cost: 12},
-   
- ];
- function addCost(accumulator,currentVal){
-      return accumulator + currentVal.cost;
- }
+// sort()
+let result1 = numbers.sort();
 
- 
- 
- let totalCost = shoppingCart.reduce(addCost,10);
+// find
+ let result2 = numbers.find((value) => {
+    return value > 50;
+});
 
- console.log("your total amount is:", totalCost);
+// filter
+
+let result3 = numbers.filter((value) => {
+    return value > 50;
+});
+
+// map
+let result4= numbers.map((value) => {
+    return  "#" + value ;
+});
+
+// redue
+
+let result5= numbers.reduce((acc, value) => {
+    return  acc * value ;
+});
+
+// map2
+let result6 = number.map((acc,value) => {
+    return acc * value;
+});
+
+
+
+
+
+
+
+console.log(result1); 
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+console.log(result6);
+
