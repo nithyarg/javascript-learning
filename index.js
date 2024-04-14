cb5f566e41af53601281857c24f33124c7e3a99f
nithyarg <nithyaasbsc@gmail.com>
2024-04-14 13:08:51 +0530

    61 completed

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-// testing a javascript array with examples
- // Allow only adults above 18
+// testing is it odd or even 
+//const numbers = [2,4,6];
 
-const queue_by_age = [24,32,45];
+// every()
+// let isEven = numbers.every(function (value){
+//     return value % 2 === 0;
+// });
 
-// let isAllowed = queue_by_age.every(function (value,index,array){
-//     console.log(`value: ${value}`);
-//     console.log("index",index);
-//     console.log("array",array);
-// }) ;
+// console.log("isEven: ",isEven);
 
-let isAllowed = queue_by_age.every(function (value,index,array){
-    return value >= 18;
+// some
+
+const numbers = [1,2,3,4,5];
+
+let isOdd = numbers.some(function (value){
+    return value % 2 === 1;
 });
 
-console.log("isAllowed: ",isAllowed); 
-
-
-// every() => true, ella condtions true va irrukanum apa tha true o/p varum.
-// some()  => true , intha condtions la  oru number value true va irruntha ve 
-               // pothum. o/p la true varum
-                
+console.log("isOdd: ", isOdd);
